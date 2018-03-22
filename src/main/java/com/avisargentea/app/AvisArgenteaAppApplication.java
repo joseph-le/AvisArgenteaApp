1dac6a66c2fbfcb33a95668d8bf68248e69d3061
Joseph Le <joele1202@gmail.com>
2018-03-22 17:56:22 -0500

    start AvisArgentea STS

---
+++ src/main/java/com/avisargentea/app/AvisArgenteaAppApplication.java
@@ -0,0 +1,12 @@
+package com.avisargentea.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvisArgenteaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvisArgenteaAppApplication.class, args);
+	}
+}
